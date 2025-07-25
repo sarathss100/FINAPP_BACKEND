cab7c776139a4ecf83b7a5d4175ae48d2b06e4fa
sarathss100 <sarathss100@gmail.com>
2025-07-25 22:16:44 +0530

    Corrected some cors

--- src/utils/middleware/corsOptions.ts
+++ src/utils/middleware/corsOptions.ts
@@ -1,6 +1,12 @@
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: [
+        'https://finapp.my',       
+        'https://www.finapp.my',  
+        'https://finapp-frontend-k3fx.vercel.app', 
+        'http://localhost:3000',
+        'http://127.0.0.1:3000' 
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
 }
