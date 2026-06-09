aa72f1ade818def5c9780d6b96fcc81ccfc82ac9
sarathss100 <sarathssofficial100@gmail.com>
2026-06-09 15:42:02 +0530

    fix: websocket issue

--- src/sockets/socket.server.ts
+++ src/sockets/socket.server.ts
@@ -1,15 +1,22 @@
-import { Server } from 'socket.io';
 import { Server as HTTPServer } from 'http';
+import { Server } from 'socket.io';
 import { setupNamespaces } from './namespaces';
 
 let io: Server;
 
 export const setupSocketIO = function(server: HTTPServer): void {
+    const socketOrigins = [
+        'https://finapp-frontend-eta.vercel.app',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000'
+    ];
+
     io = new Server(server, {
         cors: {
-            origin: '*',
+            origin: socketOrigins,
             methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-            credentials: true
+            credentials: true,
+            allowEIO3: true
         },
         path: '/socket.io',
     });
