eccf198b8960678754641e571e21db357f686c40
sarathss100 <sarathssofficial100@gmail.com>
2026-06-09 14:41:15 +0530

    fix: cors update

--- src/utils/middleware/corsOptions.ts
+++ src/utils/middleware/corsOptions.ts
@@ -5,7 +5,8 @@ const corsOptions = {
         'https://www.finapp.my',  
         'https://finapp-frontend-eta.vercel.app/', 
         'http://localhost:3000',
-        'http://127.0.0.1:3000'
+        'http://127.0.0.1:3000',
+        'https://finapp-frontend-eta.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true
